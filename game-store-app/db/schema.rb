c36c6b68c7adbfc50ac5c1b34ee08c517323354a
david <david@davids-MacBook-Pro-2.local>
2017-01-14 15:43:46 -0500

    Adding associations and creating database

--- game-store-app/db/schema.rb
+++ game-store-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114050825) do
+ActiveRecord::Schema.define(version: 20170114200516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,14 +56,19 @@ ActiveRecord::Schema.define(version: 20170114050825) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string   "status"
-    t.integer  "order_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "name"
     t.integer  "price"
     t.text     "description"
-    t.integer  "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
